59205a8c7c6986028f465d3993d4ad77558975b0
Takatoshi Matsumoto <toqoz403@gmail.com>
2013-01-17 18:33:49 +0900

    Change Pry.config.color to false while excuting command_result

--- lib/pry-command_result/kernel_ext.rb
+++ lib/pry-command_result/kernel_ext.rb
@@ -4,10 +4,13 @@ module Kernel
   def command_result(command)
     output = StringIO.new
     pager = Pry.config.pager
+    color = Pry.config.color
     Pry.config.pager = false
+    Pry.config.color = false
     Pry.run_command(command, output: output, show_output: true)
     output.string
   ensure
+    Pry.config.color = color
     Pry.config.pager = pager
     output.close
   end
