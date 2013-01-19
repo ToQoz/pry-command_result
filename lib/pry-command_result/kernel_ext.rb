7c6513f0cdf47d4fe97847519a4c1283e64ca68c
Takatoshi Matsumoto <toqoz403@gmail.com>
2013-01-19 18:54:14 +0900

    Support ruby method
    pry> command_result 'puts "hoge"'
    hoge
    => nil
    
    pry> a = "hogehgoe"
    pry> command_result 'puts a'
    hogehoge
    => nil

--- lib/pry-command_result/kernel_ext.rb
+++ lib/pry-command_result/kernel_ext.rb
@@ -1,17 +1,31 @@
 require 'stringio'
 
 module Kernel
-  def command_result(command)
-    output = StringIO.new
+  def command_result(command, target = binding)
+    is_command = Pry.commands.to_a.map(&:first).any? do |cmd|
+      case cmd
+      when Regexp
+        cmd =~ command.split(' ').first
+      when String
+        cmd == command.split(' ').first
+      end
+    end
+
     pager = Pry.config.pager
     color = Pry.config.color
     Pry.config.pager = false
     Pry.config.color = false
-    Pry.run_command(command, output: output, show_output: true)
-    output.string
+
+    if is_command
+      output = StringIO.new
+      Pry.run_command(command, output: output, show_output: true)
+      output.string
+    else
+      eval(command, target)
+    end
   ensure
     Pry.config.color = color
     Pry.config.pager = pager
-    output.close
+    output.close if output.respond_to? :close
   end
 end
